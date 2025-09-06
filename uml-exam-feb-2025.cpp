0b239dfc5f1e07d0c877be78bb829da0440ec967
Marios Grivas <148717455+Unf0undedOmn1s@users.noreply.github.com>
2025-09-06 19:50:02 +0300

    Create uml-exam-feb-2025.cpp

---
+++ uml-exam-feb-2025.cpp
@@ -0,0 +1,24 @@
+### Class Author
+| Attribute | Type   |
+|-----------|--------|
+| - name    | string |
+
+| Method                           | Return |
+|----------------------------------|--------|
+| + Author(name: string)           | ctor   |
+| + addBook(bookTitle: string)     | void   |
+| + getName()                      | string |
+| + displayBooks()                 | void   |
+
+---
+
+### Class Book
+| Attribute | Type   |
+|-----------|--------|
+| - title   | string |
+| - price   | float  |
+
+| Method                                 | Return |
+|----------------------------------------|--------|
+| + Book(title: string, price: float)    | ctor   |
+| + displayInfo()                        | void   |
