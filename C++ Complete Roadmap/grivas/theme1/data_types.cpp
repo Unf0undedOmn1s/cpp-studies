927ebf3d831807e31a2acec79b94ddeaaa5f97f2
Marios Grivas <148717455+Unf0undedOmn1s@users.noreply.github.com>
2025-11-09 20:36:54 +0200

    Update data_types.cpp

---
+++ C++ Complete Roadmap/grivas/theme1/data_types.cpp
@@ -0,0 +1,28 @@
+// int myNum = 5;               Integer
+// float myFloatNum = 5.99;     Floating point number
+// double myDoubleNum = 9.98;   Floating point number
+// char myLetter = 'D';         Character
+// bool myBoolean = true;       Boolean
+// string myText = "Hello";     String 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int myNum = 5;               // Integer
+    float myFloatNum = 5.99;     // Floating point number
+    double myDoubleNum = 9.98;   // Floating point number
+    char myLetter = 'D';         // Character
+    bool myBoolean = true;       // Boolean
+    string myText = "Hello";     // String 
+
+    cout << "Integer: " << myNum << endl;
+    cout << "Float: " << myFloatNum << endl;
+    cout << "Double: " << myDoubleNum << endl;
+    cout << "Character: " << myLetter << endl;
+    cout << "Boolean: " << myBoolean << endl;
+    cout << "String: " << myText << endl;
+
+    return 0;
+}
