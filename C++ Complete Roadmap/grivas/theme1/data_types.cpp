4dcd0c184b2c4568f530f0dac488279cc9656561
Marios Grivas <148717455+Unf0undedOmn1s@users.noreply.github.com>
2025-11-14 19:14:32 +0200

    Update data_types.cpp

--- C++ Complete Roadmap/grivas/theme1/data_types.cpp
+++ C++ Complete Roadmap/grivas/theme1/data_types.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 int main() {
-    int x = 5;               // Integer
+    int x = 5;           // Integer
     float y = 5.99;     // Floating point number
     double z = 9.98;   // Floating point number
-    char w = 'D';         // Character
-    bool v = true;       // Boolean
-    string testText = "Hello";     // String 
+    char w = 'D';     // Character
+    bool v = true;   // Boolean
+    string testText = "Hello";  // String 
 
     cout << "Integer: " << x << endl;
     cout << "Float: " << y << endl;
@@ -18,3 +18,4 @@ int main() {
     cout << "String: " << testText << endl;
     return 0;
 }
+
