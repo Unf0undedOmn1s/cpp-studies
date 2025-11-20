db0090bb62243b363dad0faa222d19b370ca4f76
Marios Grivas <148717455+Unf0undedOmn1s@users.noreply.github.com>
2025-11-20 13:49:26 +0200

    Create functions_mult.cpp

---
+++ Roadmap/grivas/theme1/functions_mult.cpp
@@ -0,0 +1,18 @@
+int add(int x, int y) {
+    return x + y;
+}
+
+int subtract(int x, int y) {
+    return x - y;
+}
+
+void showResult(int value) {
+    cout << "Result: " << value << endl;
+}
+
+int main() {
+    int a = add(10, 5);
+    showResult(a);
+    int b = subtract(10, 5);
+    showResult(b);
+}
