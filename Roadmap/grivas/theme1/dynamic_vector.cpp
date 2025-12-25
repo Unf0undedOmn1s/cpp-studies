32af49f2ae91707951708af2ad58089097bf39d8
Marios Grivas <148717455+Unf0undedOmn1s@users.noreply.github.com>
2025-12-25 16:20:18 +0200

    Add dynamic_vector.cpp for dynamic number storage

---
+++ Roadmap/grivas/theme1/dynamic_vector.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+using namespace std;
+
+// No size needed. Starts as empty.
+vector<int> numbers; 
+
+// Adding items/numbers/etc dynamically
+numbers.push_back(10); 
+numbers.push_back(20);
